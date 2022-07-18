08a7800fbb457655ca1a758a68697907247ce31a
Alejandro Garcia Vera <alejandrogarciav96@gmail.com>
2022-07-18 18:57:18 +0200

    Refactor to remove unnecessary variable onHold

--- Lecture_3/Tasks.cs
+++ Lecture_3/Tasks.cs
@@ -4,8 +4,7 @@ public static class Tasks
 {
     public static IEnumerable<int> GetFibonacci(int number)
     {
-        int prev1 = 0, prev2 = 0, index = 0, onHold;
-        
+        int prev1 = 0, prev2 = 0, index = 0;
         return Enumerable
             .Repeat(-1, number)
             .Select(num =>
@@ -15,9 +14,8 @@ public static class Tasks
                         0 or 1 => 1,
                         _ => prev2 + prev1
                     };
-                    onHold = num;
                     prev2 = prev1;
-                    prev1 = onHold;
+                    prev1 = num;
                     return num;
                 });
     }
